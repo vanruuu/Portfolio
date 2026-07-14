6b029a2208cb31544a06d96a3561e7d29f066abf
vanruuu <realraghav45@gmail.com>
2026-07-14 18:07:27 +0530

    feat: assemble portfolio sections in App

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import About from "./components/About"
+import Experience from "./components/Experience"
+import Skills from "./components/Skills"
+import Education from "./components/Education"
+import Contact from "./components/Contact"
+import Footer from "./components/Footer"
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <Hero />
+      <About />
+      <Experience />
+      <Skills />
+      <Education />
+      <Contact />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
